67ff30b41e9bca7c73b93b49c20caf4a681479f1
robertrowe1013 <robertrowe1013@gmail.com>
2020-09-09 11:04:05 +0000

    line 28, user_id

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -25,7 +25,7 @@ def all_reviews(place_id):
         data = request.get_json()
         if not request.is_json:
             abort(400, description="Not a JSON")
-        if 'user' not in request.json:
+        if 'user_id' not in request.json:
             abort(400, description="Missing user_id")
         user_id = data['user_id']
         user = storage.get(User, user_id)
